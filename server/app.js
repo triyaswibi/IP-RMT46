45711e699e3603d1106147d10cf517e833842d54
triyaswibi <triyaswii@gmail.com>
2024-03-15 14:32:56 +0700

    refactor: app

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ if(process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 
+const cors = require('cors')
 const express = require("express");
 const router = require("./routers");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 
