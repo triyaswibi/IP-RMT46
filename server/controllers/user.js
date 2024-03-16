4292a364682a83158c942df07c066d3832987f5a
triyaswibi <triyaswii@gmail.com>
2024-03-17 00:07:56 +0700

    feat: client and server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,79 +1,93 @@
-const { User } = require('../models')
-const { comparePassword } = require('../helpers/bcrypt')
-const { signToken } = require('../helpers/jwt')
+const { User } = require("../models");
+const { comparePassword } = require("../helpers/bcrypt");
+const { signToken } = require("../helpers/jwt");
 const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client();
 
 module.exports = class userController {
-    static async registerUser(req, res, next) {
-        try {
-            const {fullName, email, password, phoneNumber, address} = req.body
-            const user = await User.create({
-                fullName, 
-                email, 
-                password, 
-                phoneNumber, 
-                address
-            })
-            res.status(201).json({
-                id: user.id,
-                email: user.email,
-                fullName: user.fullName,
-                phoneNumber: user.phoneNumber,
-                address: user.address
-            })
-        } catch (error) {
-            next(error)
-        }
+  static async registerUser(req, res, next) {
+    try {
+      const { fullName, email, password, phoneNumber, address } = req.body;
+      const user = await User.create({
+        fullName,
+        email,
+        password,
+        phoneNumber,
+        address,
+      });
+      res.status(201).json({
+        id: user.id,
+        email: user.email,
+        fullName: user.fullName,
+        phoneNumber: user.phoneNumber,
+        address: user.address,
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async loginUser(req, res, next) {
-        try {
-            const {email, password} = req.body
+  static async loginUser(req, res, next) {
+    try {
+      const { email, password } = req.body;
 
-            if(!email) throw {name: "customeError", status: 400, message: "email is required"}
-            if(!password) throw {name: "customeError", status: 400, message: "password is required"}
+      if (!email)
+        throw {
+          name: "customeError",
+          status: 400,
+          message: "email is required",
+        };
+      if (!password)
+        throw {
+          name: "customeError",
+          status: 400,
+          message: "password is required",
+        };
 
-            const user = await User.findOne({
-                where: {
-                    email: email
-                }
-            })
-            
-            if(!user || !comparePassword(password, user.password)) throw {name: "customeError", status: 400, message: "invalid email/password"}
+      const user = await User.findOne({
+        where: {
+          email: email,
+        },
+      });
 
-            res.status(200).json({access_token: signToken({ id: user.id })})
-        } catch (error) {
-            next(error)
-        }
+      if (!user || !comparePassword(password, user.password))
+        throw {
+          name: "customeError",
+          status: 400,
+          message: "invalid email/password",
+        };
+
+      res.status(200).json({ access_token: signToken({ id: user.id }) });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async googleLogin(req, res, next) {
-        const { googleToken } = req.body;
-        try {
-          const ticket = await client.verifyIdToken({
-            idToken: googleToken,
-            audience:
-              "414733913348-jgbp1cosinenhu9bknunn84ftilpsvmp.apps.googleusercontent.com",
-          });
-          const { email, fullName } = ticket.getPayload();
-          const password = Math.random().toString();
-          const [user, created] = await User.findOrCreate({
-            where: { email },
-            defaults: {
-              fullName,
-              email,
-              password,
-            },
-          });
-          console.log({ user, created }, "<<< user created googleLogin");
-    
-          const access_token = signToken({ id: user.id });
-          res
-            .status(200)
-            .json({ message: "Logged in as " + user.email, access_token });
-        } catch (error) {
-          next(error);
-        }
-      }
-}
+  static async googleLogin(req, res, next) {
+    const { googleToken } = req.body;
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken: googleToken,
+        audience:
+          "414733913348-jgbp1cosinenhu9bknunn84ftilpsvmp.apps.googleusercontent.com",
+      });
+      const { email, fullName } = ticket.getPayload();
+      const password = Math.random().toString();
+      const [user] = await User.findOrCreate({
+        where: { email },
+        defaults: {
+          fullName,
+          email,
+          password,
+        },
+      });
+
+      const access_token = signToken({ id: user.id });
+      res
+        .status(200)
+        .json({ message: "Logged in as " + user.email, access_token });
+    } catch (error) {
+      next(error);
+    }
+  }
+};
